a86178b06c62adf8b5cdf70d7a5f5db76fc19e29
Thành Quý <thanhquy1105@gmail.com>
2021-07-11 03:17:41 +0700

    fixing to deploy

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const http = require("http");
+const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
